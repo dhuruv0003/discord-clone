260178a6cdb52d44753f8b07ab493187fc13aab1
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2023-12-23 00:24:33 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontFamily:{
+    whitney:["Whitney"],
+    Ginto:["Ginto"],
+    ggSans:["ggSans"],
+  }
+}
+
